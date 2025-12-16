15cbc1bed68768ff9a5d74288578e6b7e590ee26
saiprasanna karnayana <saiprasannak2004@gmail.com>
2025-12-16 15:27:10 +0530

    bg js code

---
+++ Bg App.js
@@ -0,0 +1,8 @@
+function changeBlue(element){
+    element.style.backgroundColor='blue';
+    element.style.color='White';
+}
+function changeRed(element){
+    element.style.backgroundColor='red';
+    element.style.color='white';
+}
